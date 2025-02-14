e23ec5d5e84355a02174f90dde67c9768ead4fb1
Dzida <dominik.dzida@o2.pl>
2025-02-14 16:19:49 +0100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Graphic_Neural_Network.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
